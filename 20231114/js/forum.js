668e2b95f931e0308e39b1fba4fd58543e5ef0ab
edwin68944 <151503204+edwin68944@users.noreply.github.com>
2023-11-20 15:05:29 +0800

    Add files via upload

---
+++ 20231114/js/forum.js
@@ -0,0 +1,29 @@
+﻿
+
+// 用于提交新帖子的表单处理
+
+var form = document.querySelector('#new-post-form form');
+
+form.addEventListener('submit', function (event) {
+
+    event.preventDefault();
+
+
+
+    var titleInput = document.getElementById('post-title');
+
+    var contentInput = document.getElementById('post-content');
+
+
+
+    // 在此处可以将帖子数据发送到服务器，并进行处理
+
+
+
+    // 清空表单输入
+
+    titleInput.value = '';
+
+    contentInput.value = '';
+
+});
